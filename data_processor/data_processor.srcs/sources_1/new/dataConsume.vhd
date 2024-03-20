300f8ab734121cb7dd940cdc98f7b2e4e26a44c9
Oscar <iu22229@bristol.ac.uk>
2024-03-20 11:46:46 +0000

    Output Logic Done v1.0
    It still has a problem that to read and modify dataResults it uses a variable which is not readable from outside the process for the nextStateLogic process

--- data_processor/data_processor.srcs/sources_1/new/dataConsume.vhd
+++ data_processor/data_processor.srcs/sources_1/new/dataConsume.vhd
@@ -18,10 +18,10 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.numeric_std.all;
 use work.common_pack.all;
 
 
@@ -52,8 +52,73 @@ entity dataConsume is
 end dataConsume;
 
 architecture Behavioral of dataConsume is
-    
+    TYPE STATE_TYPE is (INIT, STORE, INDEX, REQ, GET, DONE);
+    SIGNAL curState, nextState : STATE_TYPE;      
+  
+    SIGNAL maxCount : integer;   
+    SIGNAL c : integer;
+    SIGNAL intMaxIndex : integer;
 begin
+    
+    stateLogic : process (reset, clk)
+    BEGIN
+        IF (reset = '1') THEN
+            curState <= INIT;
+        ELSIF start = '1' AND rising_edge(clk) THEN
+            curState <= nextState;
+        END IF;
+    END PROCESS;
+    
+    nextStateLogic : process(ctrlIn, curState)
+    BEGIN
+    END PROCESS;
+    
+    output : process (curState)
+    variable ctrl1 : std_logic := '0';    
+    variable results : CHAR_ARRAY_TYPE(0 to RESULT_BYTE_NUM-1);
+    BEGIN
+        IF (curState = INIT) THEN
+            ctrl1 := NOT ctrl1;
+            ctrlOut <= ctrl1;
+            dataReady <= '0';
+            seqDone <= '0';
+            c <= 0;
+            intMaxIndex <= 0;
+        ELSIF (curState = STORE) THEN
+            byte <= data;
+            dataReady <= '1';
+            results := results(1 to RESULT_BYTE_NUM-1) & data;
+            dataResults <= results;
+            c <= c +1;    
+        ELSIF (curState = INDEX) THEN
+            dataReady <= '0';
+            ctrl1 := NOT ctrl1;
+            ctrlOut <= ctrl1;
+            intMaxIndex <= c -4;
+        ELSIF (curState = GET) THEN
+            byte <= data;
+            dataReady <= '1';
+            c <= c +1;
+        ELSIF (curState = REQ) THEN
+            dataReady <= '0';
+            ctrl1 := NOT ctrl1;
+            ctrlOut <= ctrl1;
+        ELSIF (curState = DONE) THEN
+            seqDone <= '1';
+        --
+        END IF;
+    END PROCESS;
+    
+    BCDIndex : process (intMaxIndex)
+    variable temp : integer;
+    BEGIN
+        temp := intMaxIndex;
+        maxIndex(2) <= std_logic_vector(to_unsigned(temp mod 10, 4));
+        temp := temp /10;
+        maxIndex(1) <= std_logic_vector(to_unsigned(temp mod 10, 4));
+        temp := temp /10;
+        maxIndex(0) <= std_logic_vector(to_unsigned(temp mod 10, 4));
+    END PROCESS;
 
 
 end Behavioral;
